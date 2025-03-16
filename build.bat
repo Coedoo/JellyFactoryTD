9e12f9a836ea6088c651344ed28c30d2e724ae7e
Coedoo <waholder1@gmail.com>
2025-03-16 16:05:42 +0100

    Update build scripts

--- build.bat
+++ build.bat
@@ -13,23 +13,25 @@ robocopy ..\lib . /s > nul
 
 for %%a in (%*) do set "%%a=1"
 
-set flags=""
-
+set flags="-vet-shadowing"
 if "%release%" == "1" (
     echo RELEASE
     set flags=%flags% -o:speed -subsystem:windows 
 ) else (
-    rem set flags=%flags% -debug -o:none -use-separate-modules -lld -show-timings
     set flags=%flags% -debug -o:none
+    rem set flags=%flags% -debug -o:none -use-separate-modules -lld -show-timings
 )
 
+set errorlevel=0
 if not "%only_game%"=="1" (
     echo "Building Platform"
     del %exe_name%
     odin build ..\src\platform_win32 %flags% -out:%exe_name%
 )
 
-odin build ..\src\game -build-mode=dll -out="Game.dll" %flags%
+if %errorlevel% == 0 (
+    odin build ..\src\game -build-mode=dll -out="Game.dll" %flags%
+)
 
 if "%run%" == "1" if %errorlevel% == 0 if %game_running% == false (
     %exe_name%
